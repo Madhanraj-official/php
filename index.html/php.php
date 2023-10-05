aeb77b27d4d12275abc9cff590edce36bf4768f6
Madhanraj-official <128833064+Madhanraj-official@users.noreply.github.com>
2023-10-05 06:01:28 +0530

    Add files via upload

---
+++ index.html/php.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css">
+</head>
+
+<body> 
+        <div><header>
+        <h1>Madhanraj</h1>
+
+        <nav>
+                <a href="#">Home</a>
+                <a href="#">Search</a>
+                <a href="#">About</a>
+                <a href="#">Contect us</a>
+        </nav>
+
+        </header>
+        </div>
+<center>
+        <h2>Nithya akka</h2>
+        <h2>Don't feel it</h2>
+        <h2>Ninga program panna try pannunga</h2>
+        <h2>kandippa life pudikkum</h2>
+        <h2></h2>
+</center>
+<div id="php">
+        <?php
+echo "<p><b>- madhan raj</b></p>";
+
+
+
+
+?>
+</div>
+</body>
+
+</html>
---
+++ test.swift
@@ -0,0 +1,5 @@
+let items=["p":8,"q":9,"r":10]
+for item in items{
+        print(item);
+}
+
